ab517043f0c6d8597a1c7da22f149298e8287f2f
thibaultCha <thibaultcha@me.com>
2013-03-02 21:20:04 +0100

    adding a mysql feature in the counter app

--- CounterApp/server.js
+++ CounterApp/server.js
@@ -2,9 +2,20 @@ var app  = require("http").createServer(handler).listen(25000)
 io       = require('socket.io').listen(app)
 url      = require("url")
 fs       = require('fs')
-file     = "/index.html"
-counter  = 0;
+mysql    = require('mysql')
 
+file       = "/index.html"
+counter    = 0;
+connection = mysql.createConnection({
+	host     : 'localhost',
+	database : 'solib',
+	user     : 'root',
+	password : 'root', // Carefull
+});
+
+/**
+* The function used by node.js to handle a request
+*/
 function handler(request, response) {
 	var pathname = url.parse(request.url).pathname
 	if (pathname == "/supersecretfunction") {
@@ -21,11 +32,32 @@ function handler(request, response) {
 	});
 }
 
+// io.socket events
 io.sockets.on("connection", function(socket) {
-	socket.emit("update", { val: counter });
+
+	// On client connection, we must send the actual count value and it last update time
+	socket.emit("updateval", { val: counter });
+	connection.query('select timestamp from updates order by timestamp desc limit 1', function(err, rows) {
+		if (err) {
+			console.log("Error connecting to mysql on select statement. Error is: " + err)
+		} else if (rows.length > 0){
+			console.log("Last update is:" + rows[0].timestamp)
+			socket.emit("updatetime", { val: rows[0].timestamp });
+		}
+	});
+	
+	// Increment event
 	socket.on("incr", function(data) {
 		counter++
-		socket.broadcast.emit("update", { val: counter });
+		socket.broadcast.emit("updateval", { val: counter });
+		connection.query('insert into updates(timestamp) values(?)', [new Date().getTime()], function(err, result) {
+			if (err) {
+				console.log("Error connecting to mysql on insert statement. Error is: " + err)
+			} else {
+				console.log("inserted: " + result.timestamp)
+				socket.broadcast.emit("updatetime", { timestamp: result})
+			}
+		});
 	});
 });
 
