ed8355ab8acff8519c35e249eff5a87f21ce9812
thibaultCha <thibaultcha@me.com>
2013-03-03 00:27:07 +0100

    config file and mysql file to handle connection + updates table creation

--- CounterApp/server.js
+++ CounterApp/server.js
@@ -1,17 +1,13 @@
-var app  = require("http").createServer(handler).listen(25000)
-io       = require('socket.io').listen(app)
-url      = require("url")
-fs       = require('fs')
-mysql    = require('mysql')
-
+var config = require('./config')
+app        = require('http').createServer(handler).listen(config.port)
+io         = require('socket.io').listen(app)
+url        = require('url')
+fs         = require('fs')
+mysql      = require('./mysql')
 file       = "/index.html"
-counter    = 0;
-connection = mysql.createConnection({
-	host     : 'localhost',
-	database : 'solib',
-	user     : 'root',
-	password : 'root', // Carefull
-});
+counter    = 0
+
+db = mysql.connect(config.host, config.database, config.username, config.password)
 
 /**
 * The function used by node.js to handle a request
@@ -20,6 +16,7 @@ function handler(request, response) {
 	var pathname = url.parse(request.url).pathname
 	if (pathname == "/supersecretfunction") {
 		counter = 0
+		console.log("counter reset")
 	}
 
 	fs.readFile(__dirname + file, function(error, data) {
@@ -36,23 +33,23 @@ function handler(request, response) {
 io.sockets.on("connection", function(socket) {
 
 	// On client connection, we must send the actual count value and it last update time
-	socket.emit("updateval", { val: counter });
-	connection.query('select timestamp from updates order by timestamp desc limit 1', function(err, rows) {
+	socket.emit("updateval", { val: counter }); // sends to new client
+	db.query('select timestamp from updates order by timestamp desc limit 1', function(err, rows) {
 		if (err)
-			console.log("Error connecting to mysql on select statement. Error is: " + err)
+			console.log("Error connecting to mysql on select statement.\n" + err)
 		else if (rows.length > 0)
 			socket.emit("updatetime", { timestamp: rows[0].timestamp });
 	});
 	
 	// Increment event
 	socket.on("incr", function(data) {
-		socket.broadcast.emit("updateval", { val: counter++ });
+		socket.broadcast.emit("updateval", { val: counter++ }); // sends to all clients except the new connection
 		var timestamp = new Date()
-		connection.query('insert into updates(timestamp) values(?)', [timestamp], function(err, result) {
+		db.query('insert into updates(timestamp) values(?)', [timestamp], function(err, result) {
 			if (err)
-				console.log("Error connecting to mysql on insert statement. Error is: " + err)
+				console.log("Error connecting to mysql on insert statement.\n" + err)
 			else
-				socket.emit("updatetime", { timestamp: timestamp })
+				io.sockets.emit("updatetime", { timestamp: timestamp }); // sends to all clients
 		});
 	});
 
