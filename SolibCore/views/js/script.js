25cafe14115822aacc68b8bb891b3e0b84148bb3
Stéphane Vieira <vieira.stephane@gmail.com>
2013-03-22 14:33:49 +0100

    CSS + Right menu animate

--- SolibCore/views/js/script.js
+++ SolibCore/views/js/script.js
@@ -3,12 +3,12 @@ window.onload = function () {
   socket    = io.connect("http://can.vas:8080")
 
   socket.on('list_users', function (data) {
-    users.innerHTML = ''
+    users.innerHTML = '';
     for (var i=0 ; i<data.users.length ; i++) {
-      var tr = document.createElement('tr')
-      tr.id = data.users[i].id
-      tr.innerHTML = data.users[i].firstname + " " + data.users[i].lastname
-      users.appendChild(tr)
+      var tr = document.createElement('tr');
+      tr.id = data.users[i].id;
+      tr.innerHTML = data.users[i].firstname + " " + data.users[i].lastname;
+      users.appendChild(tr);
     }
   });
 
@@ -17,14 +17,17 @@ window.onload = function () {
   });
 }
 
-function coucou(element) {
-  // alert(element.id)
-  if(element.id == "popopen"){
-    element.style.right="-5px"
-    element.id = "popclose"
+function openConnectedUsers(element) {
+  if($("#popopen").length){
+    $(element).animate({
+      right: "-5px"
+    }, 500 );
+    element.id = "popclose";
   }
   else {
-    element.style.right="-205px"
-    element.id = "popopen"
+    $(element).animate({
+      right: "-205px"
+    }, 500 );
+    element.id = "popopen";
   }
 }
